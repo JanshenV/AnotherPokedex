85bbe05c4b8a0c086873355b3f2c96ec0e3e5dc6
Janshen Vernon <janshen.vbb@gmail.com>
2022-08-01 17:01:05 -0300

    adding credits a my github link to the page

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,17 @@ export default function App() {
   return (
     <>
       <Body />
+      <footer className='creditsContainer'>
+        <div>
+          Made by <a href='https://github.com/JanshenV?tab=repositories'>
+            Janshen
+          </a>
+        </div>
+
+        <div>
+          All content is ® Nintendo, Game Freak, and The Pokémon Company
+        </div>
+      </footer>
     </>
   );
 }
