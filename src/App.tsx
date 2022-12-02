ef2de8d5eb0add9debff147815af2d7e3613f142
Janshen Barreto <janshen.vbb@gmail.com>
2022-12-02 17:33:23 -0300

    add general configuration to use header component, baseUrl for absolut imports and new dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
-
+//Components
+import Header from "components/Header/header.component";
 
 export default function App() {
   return (
     <div className="App">
+      <Header />
     </div>
   );
 };
